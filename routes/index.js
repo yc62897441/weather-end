b9d28aa945cf6e378a194a3046b16deeb6da832b
yc62897441 <yc62897441@gmail.com>
2022-09-05 15:58:19 +0800

    資料庫套件安裝、設定；設定註冊users/signup路由

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,50 @@ const axios = require('axios')
 let CWBAuthorization = process.env.CWBAuthorization
 let CWBbaseURL = 'https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/'
 
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const User = db.User
+
 module.exports = (app) => {
   app.get('/api/test', async (req, res) => {
     try {
       let requestURL = `${CWBbaseURL}${req.query.dataCategory}?Authorization=${CWBAuthorization}&format=${req.query.dataType}`
       const response = await axios.get(requestURL)
       if (response.status === 200) {
-        return res.json({ status: 'success', message: 'test ok', data: { ...response.data } })
+        return res.json({ status: 'success', message: 'test ok', dataset: { ...response.data.cwbopendata.dataset } })
       } else {
         return res.status(401).json({ status: 'error', message: '無法取得氣象資料' })
       }
     } catch (error) {
       console.warn(error)
     }
+  })
+
+  app.post('/api/users/signup', (req, res) => {
+    // 取得前端表單資料
+    const { account, password } = req.body
+
+    // 表單資料有缺漏，或是password, checkPassword 不一致時，return
+    if (!account || !password) {
+      return res.status(401).json({ status: 'error', message: 'account, password 資料缺漏' })
+    }
+
+    User.findOne({ where: { account: account } })
+      .then(user => {
+        if (user) {
+          return res.json({ status: 'account_exist', message: '帳號已經存在' })
+        }
+
+        User.create({
+          account: account,
+          password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+        })
+          .then(user => {
+            return res.json({ status: 'success', message: '註冊成功' })
+          })
+      })
+      .catch(error => {
+        console.warn(error)
+      })
   })
 }
