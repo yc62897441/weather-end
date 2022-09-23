7a2ecb51896300de7898c9aebe5f75954110b924
yc62897441 <yc62897441@gmail.com>
2022-09-23 15:45:28 +0800

    try line login

--- routes/index.js
+++ routes/index.js
@@ -546,14 +546,14 @@ router.get('/api/auth/line/callback', async (req, res) => {
 
     if (req.query) {
       if (req.query.code) {
-        // const response_instance_Issue_access_token = await instance_Issue_access_token.post('/', {
-        //   'grant_type': 'authorization_code',
-        //   'code': req.query.code,
-        //   'redirect_uri': 'https://side-project-weather-end.herokuapp.com/api/auth/line/callback',
-        //   'client_id': process.env.LINE_LOGIN_CHANNEL_ID,
-        //   'client_secret': process.env.LINE_LOGIN_CHANNEL_SECRET
-        // })
-        // messages = messages + `response_instance_Issue_access_token: ${response_instance_Issue_access_token} \n`
+        const response_instance_Issue_access_token = await instance_Issue_access_token.post('/', {
+          'grant_type': 'authorization_code',
+          'code': req.query.code,
+          'redirect_uri': 'https://side-project-weather-end.herokuapp.com/api/auth/line/callback',
+          'client_id': process.env.LINE_LOGIN_CHANNEL_ID,
+          'client_secret': process.env.LINE_LOGIN_CHANNEL_SECRET
+        })
+        messages = messages + `response_instance_Issue_access_token: ${response_instance_Issue_access_token} \n`
         // messages = messages + `response_instance_Issue_access_token \n`
         // for (key in response_instance_Issue_access_token) {
         //   messages = messages + `${key}: ${response_instance_Issue_access_token[key]} \n`
