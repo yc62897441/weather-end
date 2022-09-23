23b817b231e2e71eb4a6aff89b401432a042e9f4
yc62897441 <yc62897441@gmail.com>
2022-09-23 15:23:44 +0800

    try line login

--- routes/index.js
+++ routes/index.js
@@ -523,6 +523,12 @@ router.get('/api/auth/line', async (req, res) => {
 //     successRedirect: '/api/get_current_user',
 //     failureRedirect: '/api/weather_data'
 //   }))
+const instance_Issue_access_token = axios.create({
+  baseURL: 'https://api.line.me/oauth2/v2.1/token',
+  headers: {
+    "Content-Type": " application/x-www-form-urlencoded"
+  },
+})
 router.get('/api/auth/line/callback', async (req, res) => {
   try {
     // 會到 callback 這邊
@@ -543,19 +549,22 @@ router.get('/api/auth/line/callback', async (req, res) => {
     messages = messages + `req.query.code: ${req.query.code} \n`
     messages = messages + `req.query.state: ${req.query.state} \n`
 
-    // const instance_Issue_access_token = axios.create({
-    //   baseURL: 'https://api.line.me/oauth2/v2.1/token',
-    //   headers: {
-    //     "Content-Type": " application/x-www-form-urlencoded"
-    //   },
-    // })
-    // const response_instance_Issue_access_token = await instance_Issue_access_token.post('/', {
-    //   'grant_type': 'authorization_code',
-    //   'code': req.query.code,
-    //   'redirect_uri': 'https://side-project-weather-end.herokuapp.com/api/auth/line/callback',
-    //   'client_id': process.env.LINE_LOGIN_CHANNEL_ID,
-    //   'client_secret': process.env.LINE_LOGIN_CHANNEL_SECRET
-    // })
+    if (req.query) {
+      if (req.query.code) {
+        const response_instance_Issue_access_token = await instance_Issue_access_token.post('/', {
+          'grant_type': 'authorization_code',
+          'code': req.query.code,
+          'redirect_uri': 'https://side-project-weather-end.herokuapp.com/api/auth/line/callback',
+          'client_id': process.env.LINE_LOGIN_CHANNEL_ID,
+          'client_secret': process.env.LINE_LOGIN_CHANNEL_SECRET
+        })
+        messages = messages + `response_instance_Issue_access_token \n`
+        for (key in response_instance_Issue_access_token) {
+          messages = messages + `${key}: ${response_instance_Issue_access_token[key]} \n`
+        }
+      }
+    }
+
     // messages = messages + `response_instance_Issue_access_token \n`
     // messages = messages + `access_token: ${response_instance_Issue_access_token.access_token} \n`
     // messages = messages + `expires_in: ${response_instance_Issue_access_token.expires_in} \n`
