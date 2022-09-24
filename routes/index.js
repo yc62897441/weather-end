b7811cbc02e73c95a39197b3dee21e342bdf0234
yc62897441 <yc62897441@gmail.com>
2022-09-24 14:47:17 +0800

    try line login

--- routes/index.js
+++ routes/index.js
@@ -589,18 +589,20 @@ router.get('/api/auth/line/callback', async (req, res) => {
       }
       if (req.query.state) {
         messages = messages + `req.query.state: ${req.query.state} \n`
-        const bb = await User.findOne({ where: { account: req.query.state } })
-          .then(user => {
-            if (user) {
-              for (key in user) {
-                messages = messages + `user.${key}: ${user[key]} \n`
-              }
-            }
-            return
-          })
-          .catch(error => {
-            console.log(error)
-          })
+
+        // const bb = await User.findOne({ where: { account: req.query.state } })
+        //   .then(user => {
+        //     if (user) {
+        //       for (key in user) {
+        //         messages = messages + `user.${key}: ${user[key]} \n`
+        //       }
+        //     }
+        //     return
+        //   })
+        //   .catch(error => {
+        //     console.log(error)
+        //   })
+
       }
       if (req.query.code) {
         messages = messages + `req.query.code: ${req.query.code} \n`
