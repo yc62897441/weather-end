33c5703cee54fe49e40a846de0cb2c527ab3a623
yc62897441 <yc62897441@gmail.com>
2022-09-24 11:56:06 +0800

    try line login

--- routes/index.js
+++ routes/index.js
@@ -670,7 +670,7 @@ router.get('/token', async (req, res) => {
         const data = {
           grant_type: 'authorization_code',
           code: req.query.code,
-          redirect_uri: 'https://side-project-weather-end.herokuapp.com/api/auth/line/callback',
+          redirect_uri: process.env.LINE_LOGIN_CALLBACK,
           client_id: process.env.LINE_LOGIN_CHANNEL_ID,
           client_secret: process.env.LINE_LOGIN_CHANNEL_SECRET
         }
