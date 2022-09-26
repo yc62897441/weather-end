1fe36a81dbb9ddfb9b6feb69533506f428373e91
yc62897441 <yc62897441@gmail.com>
2022-09-26 13:48:07 +0800

    區分 lineController 路由

---
+++ controllers/lineController.js
@@ -0,0 +1,158 @@
+const axios = require('axios')
+// [axios] 處理 x-www-form-urlencoded 格式問題
+// https://jeremysu0131.github.io/axios-%E8%99%95%E7%90%86-x-www-form-urlencoded-%E6%A0%BC%E5%BC%8F%E5%95%8F%E9%A1%8C/
+// axios 輸出的數據是 json 格式，若我們要轉換成 x-www-form-urlencoded 格式，則需要安裝 qs 這個額外套件
+const Qs = require('qs')
+
+const jwt = require('jsonwebtoken')
+
+const db = require('../models')
+const User = db.User
+
+const lineController = {
+  // line webhook，處理聊天室的使用者事件，如follow、unfollow、message
+  webhook: async (req, res) => {
+    try {
+      let LINE_USER_ID = req.body.events[0].source.userId
+
+      // 設定 Line business messages axios
+      let LINE_CHANNEL_TOKEN = process.env.LINE_CHANNEL_TOKEN
+      let message = ''
+      const instance = axios.create({
+        baseURL: 'https://api.line.me/v2/bot/message/push',
+        timeout: 1000,
+        headers: {
+          Authorization: `Bearer ${LINE_CHANNEL_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      })
+
+      switch (req.body.events[0].type) {
+        case 'follow':
+          message = '成功加入好友'
+          break
+        case 'unfollow':
+          // unfollow: 刪除在 User 資料表中的 LINE_USER_ID
+          message = '退訂成功'
+          User.findOne({ where: { LINE_USER_ID: LINE_USER_ID } })
+            .then(user => {
+              if (user) {
+                user.update({ LINE_USER_ID: '' })
+              }
+              return
+            })
+            .catch(error => {
+              console.log(error)
+              return
+            })
+          break
+        case 'message':
+          message = '您好~'
+          break
+      }
+
+      const LineResponse = await instance.post('/', {
+        to: LINE_USER_ID,
+        messages: [
+          {
+            "type": "text",
+            "text": message
+          }
+        ]
+      })
+      return
+    } catch (error) {
+      console.log(error)
+      return
+    }
+  },
+  // Line Login 把資料發回來
+  lineCallback: async (req, res) => {
+    try {
+      if (req.query) {
+        if (req.query.state && req.query.code) {
+          const getLineUserInfoResult = await getLineUserInfo(req.query.code, req.query.state.trim())
+        }
+      } else {
+        return res.redirect('https://yc62897441.github.io/weather-front?error')
+      }
+
+      // 成功: redirect
+      return res.redirect('https://yc62897441.github.io/weather-front/#/?success_message=Line_login_success')
+    } catch (error) {
+      console.log(error)
+      return res.redirect('https://yc62897441.github.io/weather-front/#/?error_message=Line_login_fail')
+    }
+  }
+}
+
+module.exports = lineController
+
+async function getLineUserInfo(code, state) {
+  try {
+    // 先註解起來：開發檢視用，建立處理訊息，傳送給自己的 LINE 看各階段處理狀況、資料取得情況
+    // let message = 'getLineUserInfo \n'
+
+    // 透過 code (authorization code) 向 Line platform request 使用者資料(line user)
+    const data = {
+      grant_type: 'authorization_code',
+      code: code.trim(),
+      redirect_uri: process.env.LINE_LOGIN_CALLBACK,
+      client_id: process.env.LINE_LOGIN_CHANNEL_ID,
+      client_secret: process.env.LINE_LOGIN_CHANNEL_SECRET
+    }
+
+    // [axios] 處理 x-www-form-urlencoded 格式問題
+    // https://jeremysu0131.github.io/axios-%E8%99%95%E7%90%86-x-www-form-urlencoded-%E6%A0%BC%E5%BC%8F%E5%95%8F%E9%A1%8C/
+    // axios 輸出的數據是 json 格式，若我們要轉換成 x-www-form-urlencoded 格式，則需要安裝 qs 這個額外套件
+    // 取得回傳資料
+    const response = await axios.post('https://api.line.me/oauth2/v2.1/token', Qs.stringify(data), {
+      Headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    })
+
+    // 可以從回傳資料中的 id_token 解析出 line user 的資訊，接下來把 line user id 存到資料庫中了
+    const token = response.data.id_token
+    const decoded = jwt.decode(token)
+
+    // 把 line user id 存到資料庫中
+    // state 是 A unique alphanumeric string，Line 要求我們的 app 自隨機產生。這邊我在前端把 user account 帶給 state 當成值，所以現在可以用 state 去找到 user
+    const databaseResult = await User.findOne({ where: { account: state } })
+      .then(async (user) => {
+        user.update({
+          LINE_USER_ID: decoded.sub
+        })
+        // message = message + `user.account: ${user.account} \n`
+        // message = message + `user.LINE_USER_ID: ${user.LINE_USER_ID} \n`
+        // sendLine(message)
+        return 'DONE: databaseResult'
+      })
+      .catch(async (error) => {
+        console.log(error)
+        // message = message + `User.findOne Error \n`
+        // sendLine(message)
+        return 'ERROR: databaseResult'
+      })
+    return databaseResult
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// 先註解起來：開發檢視用，建立處理訊息，傳送給自己的 LINE 看各階段處理狀況、資料取得情況
+// async function sendLine(message) {
+//   try {
+//     const LINE_USER_ID = process.env.LINE_USER_ID
+//     const LineResponse = await instance.post('/', {
+//       to: LINE_USER_ID,
+//       messages: [{
+//         "type": "text",
+//         "text": `${message}`
+//       }]
+//     })
+//     return 'DONE: sendLine'
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
+const lineController = require('../controllers/lineController')
+
 const axios = require('axios')
 let CWBAuthorization = process.env.CWBAuthorization
 let CWBbaseURL = 'https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/'
-
-// [axios] 處理 x-www-form-urlencoded 格式問題
-// https://jeremysu0131.github.io/axios-%E8%99%95%E7%90%86-x-www-form-urlencoded-%E6%A0%BC%E5%BC%8F%E5%95%8F%E9%A1%8C/
-// axios 輸出的數據是 json 格式，若我們要轉換成 x-www-form-urlencoded 格式，則需要安裝 qs 這個額外套件
-const Qs = require('qs')
 
 const bcrypt = require('bcryptjs')
 const { Op } = require("sequelize")
@@ -195,7 +192,7 @@ const id_index_table = {
   D144: 150
 }
 // 定時器
-// const clock = setInterval(fetchDataAndNotify, 10000)
+// const clock = setInterval(fetchDataAndNotify, 10800000)
 async function fetchDataAndNotify() {
   try {
     // 抓取中央氣象局資料
@@ -252,131 +249,10 @@ async function fetchDataAndNotify() {
   }
 }
 
-
 // line webhook，處理聊天室的使用者事件，如follow、unfollow、message
-router.post('/api/line_webhook', async (req, res) => {
-  try {
-    let LINE_USER_ID = req.body.events[0].source.userId
-
-    // 設定 Line business messages axios
-    let LINE_CHANNEL_TOKEN = process.env.LINE_CHANNEL_TOKEN
-    let message = ''
-    const instance = axios.create({
-      baseURL: 'https://api.line.me/v2/bot/message/push',
-      timeout: 1000,
-      headers: {
-        Authorization: `Bearer ${LINE_CHANNEL_TOKEN}`,
-        "Content-Type": "application/json",
-      },
-    })
-
-    switch (req.body.events[0].type) {
-      case 'follow':
-        message = '成功加入好友'
-        break
-      case 'unfollow':
-        // unfollow: 刪除在 User 資料表中的 LINE_USER_ID
-        message = '退訂成功'
-        User.findOne({ where: { LINE_USER_ID: LINE_USER_ID } })
-          .then(user => {
-            if (user) {
-              user.update({ LINE_USER_ID: '' })
-            }
-            return
-          })
-          .catch(error => {
-            console.log(error)
-            return
-          })
-        break
-      case 'message':
-        message = '您好~'
-        break
-    }
-
-    const LineResponse = await instance.post('/', {
-      to: LINE_USER_ID,
-      messages: [
-        {
-          "type": "text",
-          "text": message
-        }
-      ]
-    })
-    return
-  } catch (error) {
-    console.log(error)
-    return
-  }
-  return
-
-  let UserId = req.user.id
-
-  // 設定 Line business messages axios
-  const LINE_CHANNEL_TOKEN = process.env.LINE_CHANNEL_TOKEN
-  const instance = axios.create({
-    baseURL: 'https://api.line.me/v2/bot/message/push',
-    timeout: 1000,
-    headers: {
-      Authorization: `Bearer ${LINE_CHANNEL_TOKEN}`,
-      "Content-Type": "application/json",
-    },
-  })
-
-  // event 類型: follow、message、unfollow
-  if (req.body.events[0].type === 'follow') {
-    // follow: 儲存 LINE_USER_ID 到 User 資料表中，傳給使用者 "已加入好友" 訊息
-    User.findByPk(UserId)
-      .then(user => {
-        user.update({
-          LINE_USER_ID: req.body.events[0].source.userId
-        })
-          .then(async (user) => {
-            try {
-              const message = `已加入好友`
-              const LineResponse = await instance.post('/', {
-                to: user.LINE_USER_ID,
-                messages: [
-                  {
-                    "type": "text",
-                    "text": message
-                  }
-                ]
-              })
-              return
-            } catch (error) {
-              console.warn(error)
-              return
-            }
-          })
-      })
-      .catch(error => {
-        console.log(error)
-        return
-      })
-    return
-  } else if (req.body.events[0].type === 'message') {
-    // message: 沒事做
-    return
-  } else if (req.body.events[0].type === 'unfollow') {
-    // unfollow: 刪除在 User 資料表中的 LINE_USER_ID
-    User.findByPk(UserId)
-      .then(user => {
-        user.update({
-          LINE_USER_ID: ''
-        })
-          .then(() => {
-            return
-          })
-      })
-      .catch(error => {
-        console.log(error)
-        return
-      })
-    return
-  }
-  return
-})
+router.post('/api/line_webhook', lineController.webhook)
+// Line Login 把資料發回來
+router.get('/api/auth/line/callback', lineController.lineCallback)
 
 router.get('/api/weather_data', async (req, res) => {
   try {
@@ -571,111 +447,5 @@ router.post('/api/users/offNotify', authenticated, (req, res) => {
       console.log(error)
     })
 })
-
-// Line Login 把資料發回來
-router.get('/api/auth/line/callback', async (req, res) => {
-  try {
-    // 先註解起來：開發檢視用，建立處理訊息，傳送給自己的 LINE 看各階段處理狀況、資料取得情況
-    // let messages = 'path: /api/auth/line/callback \n'
-
-    // 
-    if (req.query) {
-      if (req.query.state && req.query.code) {
-        // messages = messages + `req.query.state: ${req.query.state} \n`
-        // messages = messages + `req.query.code: ${req.query.code} \n`
-        const getLineUserInfoResult = await getLineUserInfo(req.query.code, req.query.state.trim())
-        // messages = messages + `getLineUserInfoResult: ${getLineUserInfoResult} \n`
-      }
-    } else {
-      return res.redirect('https://yc62897441.github.io/weather-front?error')
-    }
-
-    // 先註解起來：開發檢視用，傳送給自己的 LINE
-    // const LINE_USER_ID = process.env.LINE_USER_ID
-    // const LineResponse = await instance.post('/', {
-    //   to: LINE_USER_ID,
-    //   messages: [{
-    //     "type": "text",
-    //     "text": `${messages}`
-    //   }]
-    // })
-
-    // 成功: redirect
-    return res.redirect('https://yc62897441.github.io/weather-front/#/?success_message=Line_login_success')
-    // return res.redirect('https://yc62897441.github.io/weather-front')
-  } catch (error) {
-    console.log(error)
-    return res.redirect('https://yc62897441.github.io/weather-front/#/?error_message=Line_login_fail')
-  }
-})
-
-async function getLineUserInfo(code, state) {
-  try {
-    // 先註解起來：開發檢視用，建立處理訊息，傳送給自己的 LINE 看各階段處理狀況、資料取得情況
-    // let message = 'getLineUserInfo \n'
-
-    // 透過 code (authorization code) 向 Line platform request 使用者資料(line user)
-    const data = {
-      grant_type: 'authorization_code',
-      code: code.trim(),
-      redirect_uri: process.env.LINE_LOGIN_CALLBACK,
-      client_id: process.env.LINE_LOGIN_CHANNEL_ID,
-      client_secret: process.env.LINE_LOGIN_CHANNEL_SECRET
-    }
-
-    // [axios] 處理 x-www-form-urlencoded 格式問題
-    // https://jeremysu0131.github.io/axios-%E8%99%95%E7%90%86-x-www-form-urlencoded-%E6%A0%BC%E5%BC%8F%E5%95%8F%E9%A1%8C/
-    // axios 輸出的數據是 json 格式，若我們要轉換成 x-www-form-urlencoded 格式，則需要安裝 qs 這個額外套件
-    // 取得回傳資料
-    const response = await axios.post('https://api.line.me/oauth2/v2.1/token', Qs.stringify(data), {
-      Headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      }
-    })
-
-    // 可以從回傳資料中的 id_token 解析出 line user 的資訊，接下來把 line user id 存到資料庫中了
-    const token = response.data.id_token
-    const decoded = jwt.decode(token)
-
-    // 把 line user id 存到資料庫中
-    // state 是 A unique alphanumeric string，Line 要求我們的 app 自隨機產生。這邊我在前端把 user account 帶給 state 當成值，所以現在可以用 state 去找到 user
-    const databaseResult = await User.findOne({ where: { account: state } })
-      .then(async (user) => {
-        user.update({
-          LINE_USER_ID: decoded.sub
-        })
-        // message = message + `user.account: ${user.account} \n`
-        // message = message + `user.LINE_USER_ID: ${user.LINE_USER_ID} \n`
-        // sendLine(message)
-        return 'DONE: databaseResult'
-      })
-      .catch(async (error) => {
-        console.log(error)
-        // message = message + `User.findOne Error \n`
-        // sendLine(message)
-        return 'ERROR: databaseResult'
-      })
-    return databaseResult
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-// 先註解起來：開發檢視用，建立處理訊息，傳送給自己的 LINE 看各階段處理狀況、資料取得情況
-// async function sendLine(message) {
-//   try {
-//     const LINE_USER_ID = process.env.LINE_USER_ID
-//     const LineResponse = await instance.post('/', {
-//       to: LINE_USER_ID,
-//       messages: [{
-//         "type": "text",
-//         "text": `${message}`
-//       }]
-//     })
-//     return 'DONE: sendLine'
-//   } catch (error) {
-//     console.log(error)
-//   }
-// }
 
 module.exports = router
